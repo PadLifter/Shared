30d2376a1740906089443544322476b723a9b423
dseeha <91114193+dseeha@users.noreply.github.com>
2023-09-04 14:04:15 +0300

    Update hottub.ino

--- hottub/hottub.ino
+++ hottub/hottub.ino
@@ -1,6 +1,10 @@
 // Home Automation - Hot tub
 // Eemeli Halme & Leevi Kinnunen
-// 2023 
+// 2023
+
+#define STATUS 1
+#define HEAT 2
+#define READY 3
 
 const int temp1_pin = A0;
 //NTC sensor constants
@@ -9,23 +13,34 @@ const float adcMax = 4096.00;
 const float invT0 = 1.00 / 298.15;
 
 // Send info about heating
-void sendInfo() {
+void sendMail(int msg_type) {
+  //Parse message based on type
+  switch (msg_type) {
+  case STATUS:
+    // statements
+    break;
+  case HEAT:
+    // statements
+    break;
+    case READY:
+    // statements
+    break;
+  default:
+    // statements
+    break;
 }
 
-// Send reminder to add wood
-void sendReminder() {
+  // Send message
+
+
 }
 
-// Notify that hot tub is ready
-void sendReady() {
-}
 
 // Measure temperature of heated water
 float measureHeatTemp() {
   uint16_t temp_adc = analogRead(temp1_pin);
   // Convert to celcius
-  float temp_c = (1.00 / (invT0 + invBeta*(log ( adcMax / (float) temp_adc - 1.00))))- 273.15;
-
+  float temp_c = (1.00 / (invT0 + invBeta * (log(adcMax / (float)temp_adc - 1.00)))) - 273.15;
   Serial.print("Heated water temp: ");
   Serial.println(temp_c);
   return temp_c;
@@ -45,6 +60,7 @@ void setup() {
 void loop() {
   //Serial.printf("ADC analog value = %d\n", measureHeatTemp());
   measureHeatTemp();
+
 
   delay(1000);  // delay in between reads for clear read from serial
 }
